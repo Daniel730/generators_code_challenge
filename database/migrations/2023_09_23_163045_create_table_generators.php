7104ef0d76f691df61aab64f5e8c7d28a82d15b1
Daniel Silva <daniel.730@outlook.com>
2023-09-26 10:44:01 +0100

    seeds and migration

---
+++ database/migrations/2023_09_23_163045_create_table_generators.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('generators', function (Blueprint $table) {
+            $table->id();
+            $table->string('internal_code');
+            $table->string('brand');
+            $table->string('model');
+            $table->unsignedBigInteger('state_id')->default(1);
+            $table->timestamps();
+
+            $table->foreign('state_id')->references('id')->on('states');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('generators');
+    }
+};
